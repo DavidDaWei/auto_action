ec1a2feb5ff966944450d86394f80519abf6fa68
DavidDaWei <s10130066@gmail.com>
2026-04-07 10:37:32 +0800

    Update datetime import and increase MAX_HISTORY

--- update_price.py
+++ update_price.py
@@ -1,11 +1,11 @@
 import requests
-import datetime
+from datetime import datetime, UTC
 import re
 import json
 import os
 
 # --- 參數調整區 ---
-MAX_HISTORY = 1000  # 保存最近 100 筆資料 (若每 5 分鐘執行一次，約可保存 8 小時走勢)
+MAX_HISTORY = 2000  # 保存最近 100 筆資料 (若每 5 分鐘執行一次，約可保存 8 小時走勢)
 # ----------------
 
 # 換算常數
@@ -188,7 +188,7 @@ def process_and_save():
             """
 
     # 6. 生成最終的 index.html
-    now_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    now_str = datetime.datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
     final_content = HTML_TEMPLATE.format(
         tw_rows=tw_rows_html,
         history_json=json.dumps(history),
